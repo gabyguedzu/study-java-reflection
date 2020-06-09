b45a8c2c4d63a826b9aa39b623fd97c6d55e9c21
Gabriela Guedes <zooiv3rde@gmail.com>
2020-06-08 21:56:23 -0300

    testando exception class not found

--- src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjeto.java
+++ src/br/com/alura/alurator/playground/reflexao/TesteInstanciaObjeto.java
@@ -4,7 +4,7 @@ import br.com.alura.alurator.playground.controle.Controle;
 
 public class TesteInstanciaObjeto {
 	
-	public static void main(String[] args) throws ClassNotFoundException {
+	public static void main(String[] args) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
 		Class<Controle> controleClass1 = Controle.class;
 		
 		Controle controle = new Controle();
@@ -12,6 +12,13 @@ public class TesteInstanciaObjeto {
 		Class<? extends Controle> controleClass2 = controle.getClass();
 		
 		Class<?> controleClasse3 = Class.forName("br.com.alura.alurator.playground.controle.Controle");
+		
+		Controle objetoControle = controleClass1.newInstance();
+		
+		Object objetoControle1 = controleClasse3.newInstance();
+		
+		System.out.println(objetoControle instanceof Controle);
+		System.out.println(objetoControle1 instanceof Controle);
 	}
 
 }
